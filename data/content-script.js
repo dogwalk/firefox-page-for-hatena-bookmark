02d19bf609243e9165d68b78cad3cd614a7eb8fe
sanemat <o.gata.ken@gmail.com>
2016-02-05 00:48:07 +0900

    feat(canonical): get canonical url

--- data/content-script.js
+++ data/content-script.js
@@ -1,2 +1,20 @@
-const canonicalUrl = 'http://example.com/canonical';
+/**
+ * @license
+ * http://upa-pc.blogspot.jp/2015/03/javascript-canonical-url-get.html
+ * Copyright © 2012-2015 Dr.ウーパ
+ *
+ * @return {?string} canonical url.
+ */
+function browserCanonicalUrl() {
+  'use strict';
+  const links = document.getElementsByTagName('link');
+  const linksLength = links.length;
+  for (let i = 0; i < linksLength; i++) {
+    if (links[i].rel && links[i].rel.toLowerCase() === 'canonical') {
+      return links[i].href;
+    }
+  }
+  return null;
+}
+const canonicalUrl = browserCanonicalUrl() || location.href;
 self.port.emit('canonicalUrl', canonicalUrl);
