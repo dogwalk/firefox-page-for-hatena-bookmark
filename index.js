2fc0cd70f819e1626f27b375c955a9dcc774ac84
sanemat <o.gata.ken@gmail.com>
2016-02-04 22:25:10 +0900

    chore(resource): add/delete resources

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ if (isFirefoxAndroid) {
   let menuId = 0;
   exports.main = (options, callback) => {// eslint-disable-line no-unused-vars
     menuId = getWindow().NativeWindow.menu.add({
-      name: 'Build Link Html',
+      name: 'Page for Hatebu',
       callback: handleClick,
     });
   };
@@ -18,12 +18,12 @@ if (isFirefoxAndroid) {
 } else {
   const { ActionButton } = require('sdk/ui/button/action');
   ActionButton({// eslint-disable-line new-cap
-    id: 'build-link-html',
-    label: 'Md',
+    id: 'page-for-hatena-bookmark',
+    label: 'Page for Hatebu',
     icon: {
-      16: './filter3-16.png',
-      32: './filter3-32.png',
-      64: './filter3-64.png',
+      16: './bookmark42-16.png',
+      32: './bookmark42-32.png',
+      64: './bookmark42-64.png',
     },
     onClick: handleClick,
   });
