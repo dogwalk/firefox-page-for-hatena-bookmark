6adecfa319010033ef055781ac156bf72652b3f4
sanemat <o.gata.ken@gmail.com>
2016-02-05 20:16:10 +0900

    chore(lint): add global variable

--- data/content-script.js
+++ data/content-script.js
@@ -1,3 +1,5 @@
+/* global browserCanonicalUrl:false */
+// browser-canonical-url https://www.npmjs.com/package/browser-canonical-url
 const canonicalUrl = browserCanonicalUrl() || location.href;
 self.port.on('getCanonicalUrl', () => {
   self.port.emit('canonicalUrl', canonicalUrl);
